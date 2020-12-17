8a9d4db6c3ed8ffef99306d53d36549898e3c2ee
Barchenko1 <pavlo.barchenko@gmail.com>
2020-12-18 00:47:50 +0200

    Lab2

---
+++ app/src/main/java/com/example/myfindword/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.myfindword;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    TextView tvWord;
+    EditText edi;
+    Button buCheck;
+    Button buNew;
+
+    String currentWord;
+    Queue<String> dictionary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvInfo = findViewById(R.id.tv_info);
+        tvWord = findViewById(R.id.tv_word);
+
+        edi = findViewById(R.id.et_guess);
+
+        buCheck = findViewById(R.id.buCheck);
+        buNew = findViewById(R.id.buNew);
+        dictionary = setUpDictionary();
+    }
+
+    public void checkWord(View view) {
+        if (edi.getText().toString().equalsIgnoreCase(currentWord)) {
+            tvInfo.setText("Correct");
+            buNew.setEnabled(true);
+            buCheck.setEnabled(false);
+        } else {
+            tvInfo.setText("Try again");
+        }
+    }
+
+
+    public void newEvent(View view) {
+        if (dictionary.isEmpty()) {
+            tvInfo.setText("You won");
+            buNew.setEnabled(false);
+        } else {
+            tvInfo.setText("");
+            buNew.setEnabled(true);
+            currentWord = dictionary.poll();
+            tvWord.setText(shuffleWord(currentWord));
+            edi.setText("");
+            buCheck.setEnabled(true);
+        }
+    }
+
+    private Queue<String> setUpDictionary() {
+        Queue<String> dictionary = new PriorityQueue<>();
+        dictionary.add("one");
+        dictionary.add("two");
+        dictionary.add("three");
+
+        return dictionary;
+    }
+
+    private String shuffleWord(String word) {
+        List<String> letters = Arrays.asList(word.split(""));
+        Collections.shuffle(letters);
+        String emptyString = "";
+        for (String letter : letters) {
+            emptyString += letter;
+        }
+        return emptyString;
+    }
+
+
+
+}
